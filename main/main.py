1451e04b4173c47c754cca3ff1164f1805b95bc0
ahum01 <adel_humayun@yahoo.co.uk>
2025-05-24 19:33:12 +0100

    changes to launch and main

--- main/main.py
+++ main/main.py
@@ -1,14 +1,35 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import logging
 import asyncio
 import sys
 import os
 from pathlib import Path
 import configparser
-
 from app.api import start_rest_api
 
-logger = logging.getLogger(__name__)
+levels = (logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL)
+log_file_path = Path(__file__).parent.parent / "logs" / "app.log"
+log_file_path.parent.mkdir(parents=True, exist_ok=True)
 
+file_handler = logging.FileHandler(log_file_path, mode='w')
+file_handler.name = "file"  # Set a custom name attribute
+
+with open(log_file_path, 'w'):
+    pass
+
+logging.basicConfig(
+    level=logging.DEBUG,  # Set the logging level
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        file_handler,  # Log to file
+        logging.StreamHandler()             # Log to console
+    ]
+)
+
+logger = logging.getLogger(__name__)
 
 def get_param()->str:
     try:
@@ -27,11 +48,15 @@ def resolve_profile_path(main_app_dir:str) -> str:
         raise  FileNotFoundError(f"No Profile found {profile_path}")
     return profile_path
 
+def init_app(main_directory: str, logging_bootstrap: bool = True)->None:
+    profile_name = resolve_profile_path(main_directory)
 
 def main_app():
-    start_rest_api("debug")
+    print("Starting main app")
+    start_rest_api("DEBUG")
 
 def main():
+    logger.info("Starting application...")
     try:
         main_app()
     except Exception as ex:
