3eed1428db08cc99761f9f5da8b89c12c2babb28
Adel Humayun <80922191+ahum01@users.noreply.github.com>
2025-05-24 17:48:11 +0100

    Add files via upload

--- app/api.py
+++ app/api.py
@@ -1,17 +1,20 @@
-import time
-
-import uvicorn
 import asyncio
+import time
+import uvicorn
 import logging
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, status, Depends
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from pathlib import Path
 from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.gzip import GZipMiddleware
-
 from starlette.responses import FileResponse, PlainTextResponse, HTMLResponse
+from starlette.websockets import WebSocketDisconnect
+
+from app.lib.extract import extract_curve_metrics, extract_multi_curves, run_loop
+from cache.local_cache import ResultsCache
 
 logger = logging.getLogger(__name__)
 
@@ -28,6 +31,7 @@ def make_process_time_middleware():
 @asynccontextmanager
 async def start_api(api_app: FastAPI):
     logger.info(f"Starting API with {api_app.title}")
+    asyncio.create_task(run_loop())
     logger.info(f"API version starting : {api_app.version}")
     yield
     logger.info(f"API version shutting down")
@@ -52,16 +56,80 @@ def start_rest_api(log_level):
                        allow_credentials=True,)
     app.add_middleware(GZipMiddleware, minimum_size=10000)
 
-    uvicorn.run(app, host=host, port=port, log_level=log_level.lower(),ws_ping_timeout=None,log_config=None)
+    uvicorn.run(app, host=host, port=int(port),ws_ping_timeout=None,log_config=None)
 
 @app.get("/", response_class=HTMLResponse)
 def home():
     return "<h1> STATUS </h1>"
 
 @app.get("/log", response_class=HTMLResponse)
-def home():
+def log():
     for h in logging.getLogger().handlers:
         if h.name == "file":
             return FileResponse(h.baseFilename)
 
     return PlainTextResponse("no file logger configured")
+
+@app.get("/analytic_build_curve", response_class=HTMLResponse)
+def extract_analytic_build_curve(curve_name: str = "SONIA"):
+    df = extract_curve_metrics()
+    return JSONResponse(
+        content = jsonable_encoder(
+            df.fillna("").to_dict(orient="records")
+        )
+    )
+
+@app.get("/multi_analytic_build_curve", response_class=HTMLResponse)
+async def extract_multi_analytic_build_curve():
+    df  = await extract_multi_curves()
+    return JSONResponse(
+        content = jsonable_encoder(
+            df.fillna("").to_dict(orient="records")
+        )
+    )
+
+import pandas as pd
+from fastapi import WebSocket
+from typing import List
+from datetime import datetime
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] =[]
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        await asyncio.gather(
+            *[connection.send_text(message) for connection in self.active_connections]
+        )
+
+manager = ConnectionManager()
+# conda activate adel_tst
+# python -m websocket ws://localhost:8000/ws
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    client_id = 10
+    await manager.connect(websocket)
+    try:
+        while True:
+            df = pd.DataFrame(ResultsCache().get_all_results())
+            df["TimeStame"] = datetime.now().isoformat()
+            json_str = df.to_json(orient="records")
+            await manager.send_message(json_str, websocket)
+            await asyncio.sleep(10)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client {client_id} disconnected ")
+    except Exception as ex:
+        print(f"Error: {ex}")
+        manager.disconnect(websocket)
